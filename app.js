3c0f30f68fe8395d1baf275538e84d12db8fb28c
Adrouy <adro.alvarez90@gmail.com>
2023-01-31 23:30:05 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi, my name is Adro and I want and I am going to be a DevOps Engineer'));
+app.get('/', (req, res) => res.send('Hi, my name is Adro and I want and I am going to be a DevOps Engineer'/n 'Hi again'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
